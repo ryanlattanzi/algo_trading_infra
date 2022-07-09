3949160a13fca2873f6e7f6066ab0e5917544191
Ryan Lattanzi <ryanlattanzi7@gmail.com>
2022-07-09 12:02:46 -0400

    creating dev backend

---
+++ dev/backend/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  backend "s3" {
+    # S3 to store backend infra
+    bucket = "terraform-state-dev-596964673232"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    # Dynamo table for locking
+    dynamodb_table = "terraform-state-dev-lock-table"
+    encrypt        = true
+  }
+}
+
+module "terraform_state_dev_bucket" {
+  source                  = "terraform-aws-modules/s3-bucket/aws"
+  version                 = "3.3.0"
+  bucket                  = "terraform-state-dev-596964673232"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  versioning = {
+    status = "Enabled"
+  }
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = module.terraform_state_dev_bucket.s3_bucket_id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+module "terraform_state_dev_lock_table" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
+  version  = "2.0.0"
+  name     = "terraform-state-dev-lock-table"
+  hash_key = "LockID"
+
+  attributes = [
+    {
+      name = "LockID"
+      type = "S"
+    }
+  ]
+}
---
+++ dev/backend/outputs.tf
@@ -0,0 +1,9 @@
+output "tfstate_dev_bucket" {
+  value       = module.terraform_state_dev_bucket.s3_bucket_id
+  description = "S3 bucket that holds tfstate for dev modules."
+}
+
+output "tfstate_dev_dynamo_table" {
+  value       = module.terraform_state_dev_lock_table.dynamodb_table_id
+  description = "Dynamo DB lock table for dev tfstate."
+}
